e8963c93abb1dd0fd2b855904265db44e4bff4b5
bhargavi1poyekar <bhargavi1poyekar@gmail.com>
2023-07-18 12:53:31 -0400

    Directory

--- ansible/ansible.py
+++ ansible/ansible.py
@@ -39,6 +39,7 @@ def user_csv_process(input_file):
             add_host_name=['crange1@'+ip for ip in add_host_name]
             add_users=row[4].split('\n')
             add_users_list=json.dumps(add_users)
+            # print(add_users_list)
             user_add(add_users_list,add_host_name)
             
         
@@ -79,22 +80,65 @@ def restart(host_name):
     
     ansible_run(playbook_path,extra_vars)
 
-def create_dir(input_file,host_name):
+def dir_csv_process(input_file):
+
+    df_dir=pd.read_csv(input_file)
+    df_dir=df_dir.fillna('')
+
+    for index, row in df_dir.iterrows():
+        
+        if row[4]=='Create Directory':
+            create_host_name=[r.strip() for r in row[3].split(',')] 
+            create_host_name=['crange1@'+ip for ip in create_host_name]
+            create_path=[r.strip() for r in row[5].split(',')]
+            dir_permission=[r.strip() for r in row[6].split(',')]
+            create_dir_list=[]
+            for path,perm in zip(create_path, dir_permission):
+                if perm!='':
+                    create_dir_list.append(f'{path},{perm}')
+                else:
+                    create_dir_list.append(f'{path},0755')
+        
+            create_dir_list=json.dumps(create_dir_list)
+            # create_dir(create_dir_list,create_host_name)
+
+        elif row[4]=='Delete Directory':
+            delete_host_name=[r.strip() for r in row[3].split(',')]
+            delete_host_name=['crange1@'+ip for ip in delete_host_name]
+            delete_path=[r.strip() for r in row[5].split(',')] 
+            delete_dir_list=json.dumps(delete_path)
+            # print(delete_dir)
+            delete_dir(delete_dir_list,delete_host_name)
+    
+        # if row[4]!='':
+        #     add_host_name=['crange1@'+ip for ip in add_host_name]
+        #     add_users=row[4].split('\n')
+        #     add_users_list=json.dumps(add_users)
+        #     user_add(add_users_list,add_host_name)
+            
+        
+        # if row[6]!='':
+        #     remove_users=row[6].split('\n')
+        #     remove_users_list=json.dumps(remove_users)
+        #     remove_host_name=['crange1@'+ip for ip in remove_host_name]
+        #     user_remove(remove_users_list,remove_host_name)
+
+def create_dir(create_dir_list,create_host_name):
     playbook_path='/home/bhargavi/Cyber/ansible/playbooks/create_directory.yml'
 
     extra_vars={
-        'host_name':host_name,
-        'csv_file_path':input_file
+        'host_name':create_host_name,
+        'create_dir_list':create_dir_list
     }
     
     ansible_run(playbook_path,extra_vars)
 
-def delete_dir(input_file,host_name):
+def delete_dir(delete_dir_list,delete_host_name):
     playbook_path='/home/bhargavi/Cyber/ansible/playbooks/del_dir.yml'
 
     extra_vars={
-        'host_name':host_name,
-        'csv_file_path':input_file
+        'host_name':delete_host_name,
+        'delete_dir_list':delete_dir_list
     }
     
     ansible_run(playbook_path,extra_vars)
@@ -174,7 +218,8 @@ host_name='vmservers'
 # cmd_file='/home/bhargavi/Cyber/ansible/csv_files/cmd.csv'
 # copy_file='/home/bhargavi/Cyber/ansible/csv_files/copy.csv'
 # install_packages(install_file,host_name)
-user_csv_process('Users.csv')
+# user_csv_process('Users.csv')
+dir_csv_process('Dir.csv')
 # user_remove(remove_file,host_name)
 # restart(host_name)
 # create_dir(dir_file,host_name)
