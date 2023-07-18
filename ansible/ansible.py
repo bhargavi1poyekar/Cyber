4a976e3653c728b78313b657f53d2ebd3621bf74
bhargavi <bhargavi1poyekar>
2023-07-18 13:45:10 -0400

    power-off loop

--- ansible/ansible.py
+++ ansible/ansible.py
@@ -39,10 +39,9 @@ def user_csv_process(input_file):
             add_host_name=['crange1@'+ip for ip in add_host_name]
             add_users=row[4].split('\n')
             add_users_list=json.dumps(add_users)
-            # print(add_users_list)
             user_add(add_users_list,add_host_name)
-            
-        
+
+
         if row[6]!='':
             remove_users=row[6].split('\n')
             remove_users_list=json.dumps(remove_users)
@@ -50,14 +49,14 @@ def user_csv_process(input_file):
             user_remove(remove_users_list,remove_host_name)
 
 def user_add(add_users_list, add_host_name):
-    
+
     playbook_path='/home/bhargavi/Cyber/ansible/playbooks/user_add.yml'
 
     extra_vars={
         'host_name':add_host_name,
         'add_users_list':add_users_list
     }
-    
+
     ansible_run(playbook_path,extra_vars)
 
 def user_remove(remove_users_list, remove_host_name):
@@ -67,8 +66,22 @@ def user_remove(remove_users_list, remove_host_name):
         'host_name':remove_host_name,
         'remove_users_list':remove_users_list
     }
-    
+
     ansible_run(playbook_path,extra_vars)
+
+def power_csv_process(input_file):
+
+    machine_list=['bhar-kali','bhar-ub22']
+    machine_list=json.dumps(machine_list)
+    playbook_path='/home/bhargavi/Cyber/ansible/playbooks/power_off.yml'
+    extra_vars={
+        'machine_list':machine_list
+    }
+    options={
+        'extravars': extra_vars
+    }
+    
+    run(playbook=playbook_path, **options)
 
 def restart(host_name):
 
@@ -77,7 +90,7 @@ def restart(host_name):
     extra_vars={
         'host_name':host_name,
     }
-    
+
     ansible_run(playbook_path,extra_vars)
 
 def dir_csv_process(input_file):
@@ -86,7 +99,7 @@ def dir_csv_process(input_file):
     df_dir=df_dir.fillna('')
 
     for index, row in df_dir.iterrows():
-        
+
         if row[4]=='Create Directory':
             create_host_name=[r.strip() for r in row[3].split(',')] 
             create_host_name=['crange1@'+ip for ip in create_host_name]
@@ -98,30 +111,16 @@ def dir_csv_process(input_file):
                     create_dir_list.append(f'{path},{perm}')
                 else:
                     create_dir_list.append(f'{path},0755')
-        
+
             create_dir_list=json.dumps(create_dir_list)
-            # create_dir(create_dir_list,create_host_name)
+            create_dir(create_dir_list,create_host_name)
 
         elif row[4]=='Delete Directory':
             delete_host_name=[r.strip() for r in row[3].split(',')]
             delete_host_name=['crange1@'+ip for ip in delete_host_name]
             delete_path=[r.strip() for r in row[5].split(',')] 
             delete_dir_list=json.dumps(delete_path)
-            # print(delete_dir)
             delete_dir(delete_dir_list,delete_host_name)
-    
-        # if row[4]!='':
-        #     add_host_name=['crange1@'+ip for ip in add_host_name]
-        #     add_users=row[4].split('\n')
-        #     add_users_list=json.dumps(add_users)
-        #     user_add(add_users_list,add_host_name)
-            
-        
-        # if row[6]!='':
-        #     remove_users=row[6].split('\n')
-        #     remove_users_list=json.dumps(remove_users)
-        #     remove_host_name=['crange1@'+ip for ip in remove_host_name]
-        #     user_remove(remove_users_list,remove_host_name)
 
 def create_dir(create_dir_list,create_host_name):
     playbook_path='/home/bhargavi/Cyber/ansible/playbooks/create_directory.yml'
@@ -130,7 +129,6 @@ def create_dir(create_dir_list,create_host_name):
         'host_name':create_host_name,
         'create_dir_list':create_dir_list
     }
-    
     ansible_run(playbook_path,extra_vars)
 
 def delete_dir(delete_dir_list,delete_host_name):
@@ -140,7 +138,7 @@ def delete_dir(delete_dir_list,delete_host_name):
         'host_name':delete_host_name,
         'delete_dir_list':delete_dir_list
     }
-    
+
     ansible_run(playbook_path,extra_vars)
 
 def run_cmds(input_file,host_name):
@@ -150,7 +148,7 @@ def run_cmds(input_file,host_name):
         'host_name':host_name,
         'csv_file_path':input_file
     }
-    
+
     ansible_run(playbook_path,extra_vars)
 
 def copy_files(input_file,host_name):
@@ -160,7 +158,7 @@ def copy_files(input_file,host_name):
         'host_name':host_name,
         'csv_file_path':input_file
     }
-    
+
     ansible_run(playbook_path,extra_vars)
 
 def ufw_ssh(host_name):
@@ -169,7 +167,7 @@ def ufw_ssh(host_name):
     extra_vars={
         'host_name':host_name,
     }
-    
+
     ansible_run(playbook_path,extra_vars)
 
 def ufw_before_backup(host_name):
@@ -178,7 +176,7 @@ def ufw_before_backup(host_name):
     extra_vars={
         'host_name':host_name,
     }
-    
+
     ansible_run(playbook_path,extra_vars)
 
 def ufw_reset(host_name):
@@ -187,7 +185,7 @@ def ufw_reset(host_name):
     extra_vars={
         'host_name':host_name,
     }
-    
+
     ansible_run(playbook_path,extra_vars)
 
 def xrdp_install(host_name):
@@ -196,7 +194,7 @@ def xrdp_install(host_name):
     extra_vars={
         'host_name':host_name,
     }
-    
+
     ansible_run(playbook_path,extra_vars)
 
 
@@ -218,8 +216,11 @@ host_name='vmservers'
 # cmd_file='/home/bhargavi/Cyber/ansible/csv_files/cmd.csv'
 # copy_file='/home/bhargavi/Cyber/ansible/csv_files/copy.csv'
 # install_packages(install_file,host_name)
+
 # user_csv_process('Users.csv')
-dir_csv_process('Dir.csv')
+# dir_csv_process('Dir.csv')
+power_csv_process('Dir.csv')
+
 # user_remove(remove_file,host_name)
 # restart(host_name)
 # create_dir(dir_file,host_name)
